2e0e6c998c32e6da9fbae8eb4dd9d4fbfe1575e1
Eduardo Herrera <duds.herrera@gmail.com>
2022-07-03 14:35:53 -0300

    Adicionar arquivos de projeto.

---
+++ logica 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logica_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int cont;
+            cont = 0;
+            while (cont < 100)
+            {
+
+                cont++;
+                cont++;
+                Console.WriteLine(cont);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
